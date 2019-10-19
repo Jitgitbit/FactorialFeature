a37195621bcaba6618a25297e37e584bd6ba30b3
Thierry Dekelver <rowowolf@gmail.com>
2019-10-19 20:22:03 +0200

    first test added

--- test/index_test.js
+++ test/index_test.js
@@ -3,6 +3,11 @@ var Calculate =  require('../index.js')
 
 describe('Calculate', () => {
   describe('.factorial', () => {
-
+    it('testing if 5! gives 120', ()=>{
+      const expected = 120;
+      const inputNumber = 5;
+      const actual = Calculate.factorial(inputNumber);
+      assert.equal(expected, actual);
+    });
   });
 });
